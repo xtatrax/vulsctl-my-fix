3c23f0ced26b9fd4b2124a42b898a84cada052e2
xtatrax <xtatrax@me.com>
2025-07-14 07:28:06 +0900

    updateを少し削ってみた

---
+++ install-host/update-low-storage-pi-tuned.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#./oval.sh --redhat && \
+#./oval.sh --amazon && \
+./oval.sh --debian && \
+./oval.sh --ubuntu && \
+#./oval.sh --alpine && \
+./oval.sh --oracle && \
+#./oval.sh --fedora && \
+#./oval.sh --suse && \
+#./gost.sh --redhat && \
+./gost.sh --debian && \
+./gost.sh --ubuntu && \
+./gost.sh --microsoft && \
+./cvedb.sh --nvd && \
+./cvedb.sh --jvn && \
+./cvedb.sh --fortinet && \
+./cvedb.sh --mitre && \
+./cvedb.sh --paloalto && \
+./cvedb.sh --cisco && \
+./exploitdb.sh && \
+./msfdb.sh && \
+./kev.sh && \
+./cti.sh
